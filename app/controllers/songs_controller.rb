d69c1bb76fdf216a814e2a677b691bb65f64cead
Jamilya Ramos-Chapman <jamilya_ramos-chapman@alumni.brown.edu>
2018-10-26 17:21:43 +0000

    automatically backed up by learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -13,10 +13,11 @@ class SongsController < ApplicationController
 
   def show
     if params[:artist_id]
-      if Artist.find_by(id: params[:artist_id])
+      @artist = Artist.find_by(id: params[:artist_id])
+      if @artist
         @song = Artist.find(params[:artist_id])
       else
-        redirect_to artist_songs_path()
+        redirect_to artist_songs_path(@artist)
       end
     else
       @song = Song.find(params[:id])
